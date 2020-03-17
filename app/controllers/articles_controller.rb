96dfb74cc3ce2879432551f80026963c65de223e
KathleenScriver <klescriver@gmail.com>
2020-03-17 10:25:12 -0600

    update cors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,20 @@
 class ArticlesController < ApplicationController
-  
+  def index
+    articles = Article.all
+    render json: articles
+  end
+
+  def show
+    article = Article.find(params[:id])
+    render json: article
+  end
+
+  def create
+    article = Article.create(
+        title: params([:title]),
+        author_id: params([:author_id]),
+        magazine_id: params([:magazine_id])
+    )
+    redirect_to 'http://localhost:3001/articles.html'
+  end
 end
--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -1,6 +1,6 @@
 class MagazinesController < ApplicationController
     def index
         magazines = Magazine.find_mags(params[:category])
-        render json: magazines, include: [:articles,]
+        render json: magazines, include: [:articles]
     end
 end
